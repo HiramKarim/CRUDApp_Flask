732fa7426000705ca56ddad4053d49f8ab894f7d
hiram castro <hiram2027@gmail.com>
2021-03-19 00:43:07 -0700

    improvements
    + creating blueprints for home, auth and admin panels
    - removing useless code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from config import app_config
 from flask_login import LoginManager
 from flask_migrate import Migrate
 
@@ -9,22 +8,9 @@ db = SQLAlchemy()
 
 login_manager = LoginManager()
 
-# def create_app(config_name):
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_object(app_config[config_name])
-#     app.config.from_pyfile('config.py')
-#     db.init_app(app)
-
-#     # temporary route
-#     @app.route('/')
-#     def hello_world():
-#         return 'Hello, World!'
-
-#     return app
-
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('myconfig.DevConfig')
+    app.config.from_object('config.DevConfig')
     db.init_app(app)
 
     login_manager.init_app(app)
@@ -34,5 +20,14 @@ def create_app():
     migrate = Migrate(app, db)
 
     from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     return app
--- config.py
+++ config.py
@@ -1,28 +1,28 @@
-class Config(object):
-    """
-    Common configurations
-    """
+from os import environ, path
+from dotenv import load_dotenv
 
-    # Put any configurations here that are common across all environments
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
-
-class DevelopmentConfig(Config):
-    """
-    Development configurations
-    """
-
-    DEBUG = True
-    SQLALCHEMY_ECHO = True
-
-
-class ProductionConfig(Config):
-    """
-    Production configurations
-    """
-
+class Config:
+    """Base config."""
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SESSION_COOKIE_NAME = os.environ.get('SESSION_COOKIE_NAME')
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    
+    
+class ProdConfig(Config):
+    FLASK_ENV = 'production'
     DEBUG = False
+    TESTING = False
+    # DATABASE_URI = os.environ.get('PROD_DATABASE_URI')
 
-app_config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig
-}
+
+class DevConfig(Config):
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+    SECRET_KEY = 'p9Bv<3Eid9%$i01'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/democrudb'
+    # DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
